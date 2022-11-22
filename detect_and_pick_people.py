ed1e0ff47389b59ab00df387e094ec0171a49eb7
Ainul Yaqin <Akinpasha82@gmail.com>
2022-11-22 09:22:56 +0700

    update code

--- detect_and_pick_people.py
+++ detect_and_pick_people.py
@@ -1,144 +1,97 @@
-from utils.torch_utils import select_device, load_classifier, time_synchronized
+import torch
+from utils.torch_utils import select_device, time_synchronized
+import base64
+import numpy as np
+from io import BytesIO
+from PIL import Image
+from dummy_data import dummy
+import time
+from utils.plots import crop_img_by_bbox
+from utils.datasets import letterbox
+from utils.general import check_img_size, non_max_suppression, scale_coords, set_logging
+from models.experimental import attempt_load
 
 
-def cropImage(weight, img_arr):
-    source, weights, view_img, save_txt, imgsz = opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
-    webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-        ('rtsp://', 'rtmp://', 'http://'))
-
-    # Directories
-    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
+def cropImage(weights, img_obj, imgsz=640, device='cpu', augment=True, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic_nms=False):
     # Initialize
     set_logging()
-    device = select_device(opt.device)
+    device = select_device(device)
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
+
     if half:
         model.half()  # to FP16
-
-    # Second-stage classifier
-    classify = False
-    if classify:
-        modelc = load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
-
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
-
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+
     t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
+
+    img_arr_result = []
+
+    for idx, key in enumerate(img_obj["gallery_images"]):
+        parse_uri = (key["image"]).split(",")[1]
+        decoded_img = base64.b64decode(parse_uri)
+
+        opened_img = Image.open(BytesIO(decoded_img))
+        img_nump_arr = np.asarray(opened_img, dtype='uint8')
+
+        # Padded resize
+        img = letterbox(img_nump_arr, imgsz, stride=stride)[0]
+
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
         img = torch.from_numpy(img).to(device)
+
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
+
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
 
         # Inference
         t1 = time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
+        pred = model(img, augment=augment)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
         t2 = time_synchronized()
 
-        # Apply Classifier
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
+        cropped_img_result = []
 
         # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
-
-            cropped_result = []
-
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        for det in pred:  # detections per image
             if len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img_nump_arr.shape).round()
 
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    cropped_img_result.append(crop_img_by_bbox(xyxy, img_nump_arr))
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        if opt.heads or opt.person:
-                            if 'head' in label and opt.heads:
-                                plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-                            if 'person' in label and opt.person:
-                                cropped_result.append(crop_img_by_bbox(xyxy, im0))
-                                # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
-                        else:
-                            plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+        # Print time (inference + NMS)
+        print(f'inference + NMS Done. ({t2 - t1:.3f}s)')
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
+        img_arr_result.append(
+            {
+                "frame": idx,
+                "timestamp": key["timestamp"],
+                "image": cropped_img_result
+            }
+        )
 
-            # print(cropped_result)
+        print(f'Done. ({time.time() - t0:.3f}s)')
 
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(0)  # 1 millisecond
+        return img_arr_result
 
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    # cv2.imwrite(save_path, im0)
-                    for crop_img in cropped_result:
-                        print(save_path)
-                        cv2.imwrite(save_path, crop_img)
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
 
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
+if __name__ == "__main__":
+    print(cropImage("crowdhuman_yolov5m.pt", dummy))
 
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        print(f"Results saved to {save_dir}{s}")
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
